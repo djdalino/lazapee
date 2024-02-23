61e4900921b47596f4b6186d902ef6167398e794
Darren James Dalino <darrenjames.dalino@gmail.com>
2024-02-24 07:35:46 +0800

    update next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
+import { fileURLToPath } from 'url';
 import path from 'path';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const nextConfig = {
   env: {
